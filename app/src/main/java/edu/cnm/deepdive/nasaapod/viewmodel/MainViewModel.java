cc68b1fe248bc990778736eda97eff0e140024ca
TraceDecker <decker1041@gmail.com>
2020-02-04 16:16:14 -0700

    auto generated files for database and SQL.

--- app/src/main/java/edu/cnm/deepdive/nasaapod/viewmodel/MainViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/viewmodel/MainViewModel.java
@@ -10,7 +10,9 @@ import androidx.lifecycle.MutableLiveData;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import edu.cnm.deepdive.nasaapod.BuildConfig;
+import edu.cnm.deepdive.nasaapod.model.dao.AccessDao;
 import edu.cnm.deepdive.nasaapod.model.dao.ApodDao;
+import edu.cnm.deepdive.nasaapod.model.entity.Access;
 import edu.cnm.deepdive.nasaapod.model.entity.Apod;
 import edu.cnm.deepdive.nasaapod.service.ApodDatabase;
 import edu.cnm.deepdive.nasaapod.service.ApodService;
@@ -41,7 +43,8 @@ public class MainViewModel extends AndroidViewModel {
     Date today = new Date();
     String formattedDate = ApodService.DATE_FORMATTER.format(today);
     try {
-      setApodDate(ApodService.DATE_FORMATTER.parse(formattedDate)); //TODO Investigate adjustment for nasa APod relevant time zone.
+      setApodDate(ApodService.DATE_FORMATTER
+          .parse(formattedDate)); //TODO Investigate adjustment for nasa APod relevant time zone.
     } catch (ParseException e) {
       throw new RuntimeException(e);
     }
@@ -60,23 +63,36 @@ public class MainViewModel extends AndroidViewModel {
     dao.select(date)
         .subscribeOn(Schedulers.io())
         .subscribe(
-            (apod) -> this.apod.postValue(apod),
+            (apod) -> {
+              this.apod.postValue(apod);
+              insertAccess(apod);
+            },
             (throwable) -> this.throwable.postValue(throwable),
             () -> nasa.get(BuildConfig.API_KEY, ApodService.DATE_FORMATTER.format(date))
                 .subscribeOn(Schedulers.io())
                 .subscribe(
                     (apod) -> dao.insert(apod)
-                    .subscribeOn(Schedulers.io())
-                    .subscribe(
-                        (id) -> {
-                          apod.setId(id);
-                          this.apod.postValue(apod);
-                        },
-                        (throwable) -> this.throwable.postValue(throwable)
-                    ),
+                        .subscribeOn(Schedulers.io())
+                        .subscribe(
+                            (id) -> {
+                              apod.setId(id);
+                              this.apod.postValue(apod);
+                              insertAccess(apod);
+                            },
+                            (throwable) -> this.throwable.postValue(throwable)
+                        ),
                     (throwable) -> this.throwable.postValue(throwable)
                 )
         );
+  }
+
+  private void insertAccess(Apod apod) {
+    AccessDao accessDao = database.getAccessDao();
+    Access access = new Access();
+    access.setApodId(apod.getId());
+    accessDao.insert(access)
+        .subscribeOn(Schedulers.io())
+        .subscribe(/* TODO Handle error result */);
   }
 
 }
